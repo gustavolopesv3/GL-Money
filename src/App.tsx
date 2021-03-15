69f3e14a50dd2def2a59c0a206b4aa12e075e080
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-03-15 18:15:13 -0500

    global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  color: #8257e6;
-`;
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div>
-      <Title>Hello world</Title>
+      <h1>Ola</h1>
+      <GlobalStyle />
     </div>
   );
 }
