309441792e164a42c315a82794bd41a991b84ca6
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-03-15 19:35:14 -0500

    api miragejs and server axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header/index';
 import { GlobalStyle } from './styles/global';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transação 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Algo',
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 export function App() {
   return (
