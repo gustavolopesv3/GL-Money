57831515c9528697fad05b7dd6f3117df01a6c36
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2021-03-17 16:46:24 -0500

    button type transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainer } from './styles'
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 
 interface NewTransactionModalProps{
@@ -25,6 +27,17 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
          <h2>Cadastrar Transação</h2>
          <input type="text" placeholder='Titulo'/>
          <input type="text" placeholder='Valor'/>
+         <TransactionTypeContainer>
+             <button type='button'>
+                <img src={incomeImg} alt="Entrada"/>
+                <span>Entrada</span>
+             </button>
+             <button type='button'>
+                <img src={outcomeImg} alt="Saida"/>
+                <span>Saida</span>
+             </button>
+             
+         </TransactionTypeContainer>
          <input type="text" placeholder='Categoria'/>
          <button type="submit"> Cadastrar</button>
 
